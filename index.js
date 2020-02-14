e42375566ef289313036d852fdc4c0152a2a5073
Benjamin Grant <benjamin@nothingrandom.com>
2020-02-14 12:22:50 +0100

    Build leadingzero nano

---
+++ index.js
@@ -0,0 +1,11 @@
+const leadingzero = (num, zero = '2', char = '0') => {
+  if (zero < 0) {
+    throw new SyntaxError('leadingzero requires a positive integer for number of leading zeros');
+  }
+
+  const prefix = num < 0 ? '-' : '';
+  const number = Math.abs(parseFloat(num));
+  return prefix + String(number).padStart(zero, char);
+};
+
+module.exports = leadingzero;
